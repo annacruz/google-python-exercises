ab5eca3d5d84f40177291cd75e59053657c4e2df
Anna Cruz <anna.cruz@gmail.com>
2015-12-22 10:20:07 -0200

    Solving list1

--- basic/list1.py
+++ basic/list1.py
@@ -22,8 +22,11 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-    # +++your code here+++
-    return
+    count = 0
+    for string in words:
+        if len(string) >= 2 and string[0] == string[-1]:
+            count += 1
+    return count
 
 
 # B. front_x
@@ -34,8 +37,16 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-    # +++your code here+++
-    return
+    xwords = []
+    owords = []
+    for string in words:
+        if string[0] == 'x':
+            xwords.append(string)
+        else:
+            owords.append(string)
+    xwords.sort()
+    owords.sort()
+    return xwords + owords
 
 
 # C. sort_last
@@ -45,9 +56,8 @@ def front_x(words):
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
 def sort_last(tuples):
-    # +++your code here+++
-    return
-
+    tuples.sort(key=lambda tup: tup[1])
+    return  tuples
 
 # Simple provided test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
@@ -68,12 +78,9 @@ def main():
 
     print()
     print('front_x')
-    test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
-         ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
-    test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
-         ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
-    test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
-         ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+    test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+    test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+    test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']), ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
 
     print()
     print('sort_last')
